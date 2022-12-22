741fdeb8d6d4e0ee4602b3766bc11ff17a12b783
Roberto Polli <robipolli@gmail.com>
2022-12-22 18:55:31 +0100

    fix uid.

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
-FROM nginx:1.15.8
+FROM nginxinc/nginx-unprivileged
 LABEL maintainer="erika.pauwels@gmail.com"
 
 ENV ENABLE_ENDPOINT_SELECTOR false
 ENV DEFAULT_SPARQL_ENDPOINT http://localhost/sparql
 
-RUN chown -R nginx:nginx /usr/share/nginx/html/ /var/cache/nginx/ /var/run/
+USER root
 COPY index.html /usr/share/nginx/html/
 COPY startup.sh /
 RUN chmod +x /startup.sh
+RUN chown 101 /usr/share/nginx/html/index.html
 
+USER 101
 ENTRYPOINT ["/startup.sh"]
 
 CMD ["nginx", "-g", "daemon off;"]
-USER nginx
--- startup.sh
+++ startup.sh
@@ -2,12 +2,14 @@
 
 if [ "$ENABLE_ENDPOINT_SELECTOR" == "true" ];
 then
-    sed -i '/\/\*\* endpoint-selector \*\//c\'"\/** .yasgui .endpointText {display:none !important;} *\/ \/** endpoint-selector *\/" /usr/share/nginx/html/index.html
+    sed -e '/\/\*\* endpoint-selector \*\//c\'"\/** .yasgui .endpointText {display:none !important;} *\/ \/** endpoint-selector *\/" /usr/share/nginx/html/index.html > /tmp/index.html && cp /tmp/index.html /usr/share/nginx/html/index.html
 else
-    sed -i '/\/\*\* endpoint-selector \*\//c\'".yasgui .endpointText {display:none !important;} \/** endpoint-selector *\/" /usr/share/nginx/html/index.html
+    sed -e '/\/\*\* endpoint-selector \*\//c\'".yasgui .endpointText {display:none !important;} \/** endpoint-selector *\/" /usr/share/nginx/html/index.html > /tmp/index.html && cp /tmp/index.html /usr/share/nginx/html/index.html
+
 fi
 
-sed -i '/default SPARQL endpoint/c\'"requestConfig: {endpoint: '$DEFAULT_SPARQL_ENDPOINT', method: 'GET'} \/\/ default SPARQL endpoint" /usr/share/nginx/html/index.html
+sed -e '/default SPARQL endpoint/c\'"requestConfig: {endpoint: '$DEFAULT_SPARQL_ENDPOINT', method: 'GET'} \/\/ default SPARQL endpoint" /usr/share/nginx/html/index.html > /tmp/index.html && cp /tmp/index.html /usr/share/nginx/html/index.html
+
 
 echo "Running with ${DEFAULT_SPARQL_ENDPOINT}"
 
